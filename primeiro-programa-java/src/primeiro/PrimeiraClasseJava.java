9c3ac7ef37ce587ff234dc9857057da4aaa57d55
Alisson Deives <alissondeives70@gmail.com>
2022-02-14 16:37:26 -0300

    Classe de média de notas

--- primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
@@ -1,9 +1,22 @@
 package primeiro;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 	
 	public static void main(String[] args) {
-
+		double n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: "));
+		double n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota: "));
+		double n3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a terceira nota: "));
+		
+		double media = (n1 + n2 + n3)/3;
+		
+ 		if(media < 7) {
+			System.out.println("A média é " + media + ", aluno reprovado!");
+		}
+		else {
+			System.out.println("A média é "+ media + ", aluno aprovado!" );
+		}
 		
 	}
 }

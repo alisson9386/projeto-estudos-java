ee8d4a7da038621960c6297106b033b903fdde3c
Alisson Deives <alissondeives70@gmail.com>
2022-02-15 08:42:46 -0300

    Switch case, operações exatas

--- primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
@@ -1,32 +1,26 @@
 package primeiro;
 
-import javax.swing.JOptionPane;
-
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		
-		double n1 = Double.parseDouble(JOptionPane.showInputDialog("Digite a primeira nota: "));
-		double n2 = Double.parseDouble(JOptionPane.showInputDialog("Digite a segunda nota: "));
-		double n3 = Double.parseDouble(JOptionPane.showInputDialog("Digite a terceira nota: "));
-		double n4 = Double.parseDouble(JOptionPane.showInputDialog("Digite a quarta nota: "));
 
-		double media = (n1 + n2 + n3 + n4) / 4;
+		/*Switch case: operações exatas*/
+		int dia = 3;
+		switch (dia) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+			
+		case 2:
+			System.out.println("Segunda");
+			break;
+			
+		case 3:
+			System.out.println("Terça");
+			break;
 
-		/*Operações lógicas aninhadas: Operações dentro de operações*/
-		
-		if(media >= 50) {
-			if(media >=70) {
-				if(media > 90) {
-					System.out.println("Aluno aprovado com média acima do comum");
-				}else {
-					System.out.println("Aluno aprovado direto");
-				}
-			}else {
-				System.out.println("Aluno em recuperação");
-			}
-		}else {
-			System.out.println("Aluno reprovado direto");
+		default: System.out.println("Outro dia qualquer");
+			break;
 		}
 		
 		

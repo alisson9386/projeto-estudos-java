b31db62ca5bac421f1fdca94dc7cf61810fe37e4
Alisson Deives <alissondeives70@gmail.com>
2022-02-16 10:47:01 -0300

    Operações relacionais

--- primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
@@ -1,26 +1,23 @@
 package primeiro;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/*Switch case: operações exatas*/
-		int dia = 3;
-		switch (dia) {
-		case 1:
-			System.out.println("Domingo");
-			break;
-			
-		case 2:
-			System.out.println("Segunda");
-			break;
-			
-		case 3:
-			System.out.println("Terça");
-			break;
-
-		default: System.out.println("Outro dia qualquer");
-			break;
+		/*Operações relacionais*/
+		int nota1 = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota 1"));
+		int nota2 = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota 2"));
+		
+		if (nota1 == nota2) {
+			JOptionPane.showMessageDialog(null, "As notas são iguais", null, JOptionPane.INFORMATION_MESSAGE);
+		}else if(nota1 != nota2){
+			JOptionPane.showMessageDialog(null, "As notas são diferentes", null, JOptionPane.INFORMATION_MESSAGE);
+		}else if(nota1 < nota2) {
+			JOptionPane.showMessageDialog(null, "Nota 1 menor que nota 2", null, JOptionPane.INFORMATION_MESSAGE);
+		}else if(nota1 > nota2) {
+			JOptionPane.showMessageDialog(null, "Nota 1 maior que nota2", null, JOptionPane.INFORMATION_MESSAGE);
 		}
 		
 		

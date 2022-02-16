7b5d9a04d4b14a4b644b6e8159a38c09fdbf05d4
Alisson Deives <alissondeives70@gmail.com>
2022-02-16 20:18:20 -0300

    Estrutura de repetição while/do while

--- primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
@@ -1,25 +1,28 @@
 package primeiro;
 
-import javax.swing.JOptionPane;
-
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/*Operações relacionais*/
-		int nota1 = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota 1"));
-		int nota2 = Integer.parseInt(JOptionPane.showInputDialog("Digite a nota 2"));
+		/*Estrutura de repetição while/do while*/
+
+		int numero = 0;
+		int numero2 = 0;
 		
-		if (nota1 == nota2) {
-			JOptionPane.showMessageDialog(null, "As notas são iguais", null, JOptionPane.INFORMATION_MESSAGE);
-		}else if(nota1 != nota2){
-			JOptionPane.showMessageDialog(null, "As notas são diferentes", null, JOptionPane.INFORMATION_MESSAGE);
-		}else if(nota1 < nota2) {
-			JOptionPane.showMessageDialog(null, "Nota 1 menor que nota 2", null, JOptionPane.INFORMATION_MESSAGE);
-		}else if(nota1 > nota2) {
-			JOptionPane.showMessageDialog(null, "Nota 1 maior que nota2", null, JOptionPane.INFORMATION_MESSAGE);
+		/*While verifica e depois executa*/
+		while(numero <= 3) {
+			System.out.println("O numero atual é: "+numero);
+			numero ++;
 		}
 		
+		/*-------------------------------------------*/
+		
+		/*Do while primeiro executa e depois verifica */
+		do {
+			System.out.println("O numero atual é: "+numero2);
+			numero2 ++;
+		}while(numero2 <= 60);
+			
 		
 	}
 }

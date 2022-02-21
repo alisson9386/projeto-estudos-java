717892211d8d3719480492ab8abe553dde5ca6c1
Alisson Deives <alissondeives70@gmail.com>
2022-02-21 09:27:13 -0300

    Entrada de dados com JOptionPane

--- primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/primeiro/PrimeiraClasseJava.java
@@ -1,17 +1,24 @@
 package primeiro;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
 
-		/*Módulo: resto da divisão %*/
+		/*Entrada de dados*/
+		
+		String numeroPessoas = JOptionPane.showInputDialog("Informe o numero de pessoas: ");
+		String numCarros = JOptionPane.showInputDialog("Informe a quantidade de carros: ");
+		
+		double carrosNumero = Double.parseDouble(numCarros);
+		double pessoaNumero = Double.parseDouble(numeroPessoas);
+		
+		int divisao = (int) (carrosNumero / pessoaNumero);
+		int resto = (int) (carrosNumero % pessoaNumero);
+		
+		//System.out.println(numeroPessoas +" pessoas\n"+ numCarros + " carros\n"+ divisao + " para cada um\n" + "Sobrou "+resto+" carro(s)");
 
-		double carros = 9;
-		double pessoa = 2;
-		double resto = carros % pessoa; /*MOD*/
-		
-		System.out.println("Sobraram " + resto + " carro(s)");
-			
-		
+		JOptionPane.showMessageDialog(null, numeroPessoas +" pessoas\n"+ numCarros + " carros\n"+ divisao + " para cada um\n" + "Sobrou "+resto+" carro(s)");
 	}
 }

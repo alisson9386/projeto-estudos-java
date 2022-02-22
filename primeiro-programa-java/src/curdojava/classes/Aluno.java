d9110f5f5444f8fc6b77ab3295c0506834da9cf7
Alisson Deives <alissondeives70@gmail.com>
2022-02-22 09:25:39 -0300

    Criando a classe aluno

---
+++ primeiro-programa-java/src/curdojava/classes/Aluno.java
@@ -0,0 +1,17 @@
+package curdojava.classes;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String dataNascimento;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+	
+
+}

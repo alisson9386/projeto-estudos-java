1047d07d4c09e654d02898d8c64627fb0a6c85d7
Alisson Deives <alissondeives70@gmail.com>
2022-02-22 10:31:05 -0300

    Getters e Setters

--- primeiro-programa-java/src/curdojava/classes/Aluno.java
+++ primeiro-programa-java/src/curdojava/classes/Aluno.java
@@ -2,28 +2,111 @@ package curdojava.classes;
 
 public class Aluno {
 
-	/*Atributos do aluno*/
-	public String nome;
-	public int idade;
-	public String dataNascimento;
-	public String registroGeral;
-	public String numeroCpf;
-	public String nomeMae;
-	public String nomePai;
-	public String dataMatricula;
-	public String nomeEscola;
-	public String serieMatriculado;
+	/* Atributos do aluno */
+	private String nome;
+	private int idade;
+	private String dataNascimento;
+	private String registroGeral;
+	private String numeroCpf;
+	private String nomeMae;
+	private String nomePai;
+	private String dataMatricula;
+	private String nomeEscola;
+	private String serieMatriculado;
 
 	public Aluno() {
-		
+
 	}
-	
+
 	public Aluno(String nomePadrao) {
 		nome = nomePadrao;
 	}
-	
+
 	public Aluno(String nomePadrao, int idadePadrao) {
 		nome = nomePadrao;
 		idade = idadePadrao;
 	}
+	
+	/* Métodos Getters e Setters do objeto */
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getRegistroGeral() {
+		return registroGeral;
+	}
+
+	public void setRegistroGeral(String registroGeral) {
+		this.registroGeral = registroGeral;
+	}
+
+	public String getNumeroCpf() {
+		return numeroCpf;
+	}
+
+	public void setNumeroCpf(String numeroCpf) {
+		this.numeroCpf = numeroCpf;
+	}
+
+	public String getNomeMae() {
+		return nomeMae;
+	}
+
+	public void setNomeMae(String nomeMae) {
+		this.nomeMae = nomeMae;
+	}
+
+	public String getNomePai() {
+		return nomePai;
+	}
+
+	public void setNomePai(String nomePai) {
+		this.nomePai = nomePai;
+	}
+
+	public String getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(String dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public String getNomeEscola() {
+		return nomeEscola;
+	}
+
+	public void setNomeEscola(String nomeEscola) {
+		this.nomeEscola = nomeEscola;
+	}
+
+	public String getSerieMatriculado() {
+		return serieMatriculado;
+	}
+
+	public void setSerieMatriculado(String serieMatriculado) {
+		this.serieMatriculado = serieMatriculado;
+	}
+	
 }
--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -11,11 +11,52 @@ public class PrimeiraClasseJava {
 		 * aluno1 é referencia para o objeto aluno*/
 		
 		Aluno aluno1 = new Aluno(); /*Joao*/
-		aluno1.nome = "Joao";
+		aluno1.setNome("Joao da Silva");
+		aluno1.setIdade(50);
+		aluno1.setDataNascimento("06/02/1997");
+		aluno1.setRegistroGeral("17756119");
+		aluno1.setNumeroCpf("13502004684");
+		aluno1.setNomeMae("Lucimar");
+		aluno1.setNomePai("Nivaldo");
+		aluno1.setDataMatricula("06/07/2021");
+		aluno1.setSerieMatriculado("6°");
+		aluno1.setNomeEscola("Prodor");
 		
-		System.out.println(aluno1.nome);
+		System.out.println("Aluno 1: "+ aluno1.getNome());
+		System.out.println("Aluno 1: "+ aluno1.getIdade());
+		System.out.println("Aluno 1: "+ aluno1.getDataNascimento());
+		System.out.println("Aluno 1: "+ aluno1.getRegistroGeral());
+		System.out.println("Aluno 1: "+ aluno1.getNumeroCpf());
+		System.out.println("Aluno 1: "+ aluno1.getNomeMae());
+		System.out.println("Aluno 1: "+ aluno1.getNomePai());
+		System.out.println("Aluno 1: "+ aluno1.getDataMatricula());
+		System.out.println("Aluno 1: "+ aluno1.getSerieMatriculado());
+		System.out.println("Aluno 1: "+ aluno1.getNomeEscola() + "\n\n\n");
+		
+		
 		
 		Aluno aluno2 = new Aluno(); /*Pedro*/
+		aluno2.setNome("Alisson Vieira");
+		aluno2.setIdade(25);
+		aluno2.setDataNascimento("06/02/1997");
+		aluno2.setRegistroGeral("17756119");
+		aluno2.setNumeroCpf("13502004684");
+		aluno2.setNomeMae("Lucimar");
+		aluno2.setNomePai("Nivaldo");
+		aluno2.setDataMatricula("06/07/2021");
+		aluno2.setSerieMatriculado("6°");
+		aluno2.setNomeEscola("Prodor");
+		
+		System.out.println("Aluno 2: "+ aluno2.getNome());
+		System.out.println("Aluno 2: "+ aluno2.getIdade());
+		System.out.println("Aluno 2: "+ aluno2.getDataNascimento());
+		System.out.println("Aluno 2: "+ aluno2.getRegistroGeral());
+		System.out.println("Aluno 2: "+ aluno2.getNumeroCpf());
+		System.out.println("Aluno 2: "+ aluno2.getNomeMae());
+		System.out.println("Aluno 2: "+ aluno2.getNomePai());
+		System.out.println("Aluno 2: "+ aluno2.getDataMatricula());
+		System.out.println("Aluno 2: "+ aluno2.getSerieMatriculado());
+		System.out.println("Aluno 2: "+ aluno2.getNomeEscola());
 		
 		Aluno aluno3 = new Aluno(); /*Alisson*/
 		

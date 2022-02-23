f41dd101eca2c89bddb3a5549d78f0861b82cfe3
Alisson Deives <alissondeives70@gmail.com>
2022-02-23 17:48:53 -0300

    Conhecendo o Equals E HashCode

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,7 +1,5 @@
 package cursojava.executavel;
 
-import javax.swing.JOptionPane;
-
 import curdojava.classes.Aluno;
 
 public class PrimeiraClasseJava {
@@ -9,10 +7,7 @@ public class PrimeiraClasseJava {
 	public static void main(String[] args) {
 		
 		
-		/*new Aluno() é uma instancia (criação de objeto)
-		 * aluno1 é referencia para o objeto aluno*/
-		
-		String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
+		/*String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 		String idade = JOptionPane.showInputDialog("Qual a idade do aluno?");
 		String dataNascimento = JOptionPane.showInputDialog("Qual a data de nascimento do aluno?");
 		String nota1 = JOptionPane.showInputDialog("1° nota");
@@ -35,7 +30,24 @@ public class PrimeiraClasseJava {
 		
 		System.out.println(aluno1.toString());
 		System.out.println("Media: "+ aluno1.getMediaNota());
-		System.out.println("Resultado: "+(aluno1.getSituacaoAluno()? "Aprovado" : "Reprovado"));
+		System.out.println("Resultado: "+(aluno1.getSituacaoAluno()? "Aprovado" : "Reprovado"));*/
+		
+		/*Equals e HashCode*/
+		
+		Aluno aluno1 = new Aluno();
+		aluno1.setNome("Alex");
+		aluno1.setNumeroCpf("13502004684");
+		
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("Alex");
+		aluno2.setNumeroCpf("14461521621");
+		
+		
+		if(aluno1.equals(aluno2)) {
+			System.out.println("Alunos são iguais");
+		}else {
+			System.out.println("Alunos não são iguais");
+		}
 		
 	}
 }

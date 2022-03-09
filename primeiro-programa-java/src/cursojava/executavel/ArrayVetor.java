d37352d0c2eead448e8ad0a2a52cf1909cfc404d
Alisson Deives <alissondeives70@gmail.com>
2022-03-09 10:39:34 -0300

    Percorrendo listas e arrays

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -4,29 +4,44 @@ import cursojava.classes.Aluno;
 import cursojava.classes.Disciplina;
 
 public class ArrayVetor {
-	
+
 	public static void main(String[] args) {
-		
-		double[] notas1 = {8.8,7.9,4.5,8.1};
-		double[] notas2 = {9.8,8.7,7.4,9.9};
-		
+
+		double[] notas1 = { 8.8, 7.9, 4.5, 8.1 };
+		double[] notas2 = { 9.8, 8.7, 7.4, 9.9 };
+
 		Aluno aluno = new Aluno();
-		
+
 		aluno.setNome("Alisson");
-		aluno.setNomeEscola("Promove");
-		
+		aluno.setNomeEscola("Jdev");
+
 		Disciplina disciplina = new Disciplina();
 		disciplina.setDisciplina("Java");
 		disciplina.setNota(notas1);
-		
+
 		aluno.getDisciplinas().add(disciplina);
-		
+
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setDisciplina("SQL");
 		disciplina2.setNota(notas2);
-		
+
 		aluno.getDisciplinas().add(disciplina2);
-		
+
+		System.out.println("Nome do aluno: " + aluno.getNome() + " do cruso " + aluno.getNomeEscola());
+		System.out.println("-------------------- Disciplinas do aluno --------------------");
+
+		for (Disciplina disc : aluno.getDisciplinas()) {
+
+			System.out.println("\nDisciplina: " + disc.getDisciplina());
+			System.out.println("Notas da disciplina: ");
+
+			for (int pos = 0; pos < disc.getNota().length; pos++) {
+
+				int sit = pos + 1;
+				System.out.println("Nota " + sit + ": " + disc.getNota()[pos]);
+			}
+		}
+
 	}
 
 }

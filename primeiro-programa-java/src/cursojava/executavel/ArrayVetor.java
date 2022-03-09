27eea02f514036f6401f057fa8cc658559c42ca8
Alisson Deives <alissondeives70@gmail.com>
2022-03-09 08:30:20 -0300

    Recuperando valores no Array

--- primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
+++ primeiro-programa-java/src/cursojava/executavel/ArrayVetor.java
@@ -6,18 +6,16 @@ public class ArrayVetor {
 		
 		/*Array pode ser de todos os tipos de dados e objetos também*/
 		
-		double[] notas = new double[4];
+		double[] notas = new double[5];
 		
 		notas[0] = 9.8;
 		notas[1] = 9.7;
 		notas[2] = 8.7;
 		notas[3] = 7.7;
 		
-		System.out.println(notas);
-		
-		
-		
-		
+		for(int i = 0; i < 5; i++ ) {
+			System.out.println("Nota " + (i + 1) + ": "+ notas[i]);
+		}
 	}
 
 }

86a31ebaf9f6fdb359e5d33fe9c3edc055634df0
Alisson Deives <alissondeives70@gmail.com>
2022-03-12 10:58:14 -0300

    Criando uma Matriz multidimensional

---
+++ primeiro-programa-java/src/cursojava/executavel/Matriz.java
@@ -0,0 +1,22 @@
+package cursojava.executavel;
+
+public class Matriz {
+	
+	public static void main(String[] args) {
+		
+		int notas[][] = new int[2][3];
+		
+		notas[0][0] = 80; 
+		notas[0][1] = 90;
+		notas[0][2] = 70;
+		
+		notas[1][0] = 50; 
+		notas[1][1] = 75;
+		notas[1][2] = 83;
+		
+		
+		System.out.println(notas);
+		
+	}
+
+}

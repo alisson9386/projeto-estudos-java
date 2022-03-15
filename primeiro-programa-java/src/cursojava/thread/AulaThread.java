0a580abb885c2aa2cf9678cb2e0b123b158b54a1
Alisson Deives <alissondeives70@gmail.com>
2022-03-15 09:14:25 -0300

    Criando a primeira Thread

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -1,21 +1,42 @@
 package cursojava.thread;
 
+import javax.swing.JOptionPane;
+
 public class AulaThread {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
-		
-		for(int i = 0; i <10; i++ ) {
+		new Thread() {
 			
+				public void run() {
+					
+					/*Código da rotina que quero executar em paralelo*/
+					for(int i = 0; i <10; i++ ) {
+						
 
-			/*Quero executar esse envio com um tempo de parada, ou com um tempo determinado*/
+						/*Quero executar esse envio com um tempo de parada, ou com um tempo determinado*/
+						
+						System.out.println("Executando envio de e-mail ao destinatario " + (i+1));
+						
+						 try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					/*Fim do codigo em paralelo*/
+					
+				}
 			
-			System.out.println("Executando envio de e-mail");
 			
-			 Thread.sleep(1000);
-		}
+		}.start(); /*Liga a Thread*/
 		
-		System.out.println("Fim do teste");
+		
+		
+		
+		System.out.println("Envios de notas fiscais realizado");
+		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuario");
 		
 	}
 

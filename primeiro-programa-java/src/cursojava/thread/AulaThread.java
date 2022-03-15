6f16ee603d73dd06cc025b737c931a4af60c7745
Alisson Deives <alissondeives70@gmail.com>
2022-03-15 09:40:13 -0300

    Processamento concorrente entre duas Thread

--- primeiro-programa-java/src/cursojava/thread/AulaThread.java
+++ primeiro-programa-java/src/cursojava/thread/AulaThread.java
@@ -6,6 +6,7 @@ public class AulaThread {
 	
 	public static void main(String[] args) throws InterruptedException {
 		
+		/*Envio de email*/
 		new Thread() {
 			
 				public void run() {
@@ -17,6 +18,37 @@ public class AulaThread {
 						/*Quero executar esse envio com um tempo de parada, ou com um tempo determinado*/
 						
 						System.out.println("Executando envio de e-mail ao destinatario " + (i+1));
+						
+						 try {
+							Thread.sleep(4000);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					/*Fim do codigo em paralelo*/
+					
+				}
+			
+			
+		}.start(); /*Liga a Thread*/
+		
+		
+		
+		/*DIVISAO DE THREADS*/
+		
+		/*Envio de nota fiscal*/
+		new Thread() {
+			
+				public void run() {
+					
+					/*Código da rotina que quero executar em paralelo*/
+					for(int i = 0; i <10; i++ ) {
+						
+
+						/*Quero executar esse envio com um tempo de parada, ou com um tempo determinado*/
+						
+						System.out.println("Executando envio de nota fiscal ao destinatario " + (i+1));
 						
 						 try {
 							Thread.sleep(1000);
@@ -32,11 +64,12 @@ public class AulaThread {
 			
 		}.start(); /*Liga a Thread*/
 		
+		JOptionPane.showMessageDialog(null, "Envios de notas fiscais realizado");
 		
+		String nome = JOptionPane.showInputDialog("Digite o seu nome");
 		
+		JOptionPane.showMessageDialog(null, "Seu nome é " + nome);
 		
-		System.out.println("Envios de notas fiscais realizado");
-		JOptionPane.showMessageDialog(null, "Sistema continua executando para o usuario");
 		
 	}
 

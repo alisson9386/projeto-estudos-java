717bcc04d09cbff2f63980f026844bd7780cedc7
Alisson Deives <alissondeives70@gmail.com>
2022-03-18 20:23:18 -0300

    Controlando a Thead 1 e Thread 2 com Start e Stop

--- primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
+++ primeiro-programa-java/src/cursojava/thread/TelaTimeThread.java
@@ -9,23 +9,60 @@ import javax.swing.plaf.DimensionUIResource;
 import javax.swing.plaf.InsetsUIResource;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class TelaTimeThread extends JDialog {
 	
 	private JPanel jPanel = new JPanel(new GridBagLayout());
 	
 	private JLabel descricaoHora = new JLabel("Time da Thread 1");
-	
 	private JTextField mostraTempo = new JTextField();
 	
 	private JLabel descricaoHora2 = new JLabel("Time da Thread 2");
-	
 	private JTextField mostraTempo2 = new JTextField();
 	
 	private JButton jButton = new JButton("Start");
-	
 	private JButton jButton2 = new JButton("Stop");
 	
+	private Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			while(true) {
+				mostraTempo.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	
+	private Runnable thread2 = new Runnable() {
+			
+			@Override
+			public void run() {
+				while(true) {
+					mostraTempo2.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss").format(Calendar.getInstance().getTime()));
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				
+			}
+		};
+	
+	
+	private Thread thread1Time;
+	private Thread thread2Time;
 
 		public TelaTimeThread() {
 
@@ -67,6 +104,31 @@ public class TelaTimeThread extends JDialog {
 			jButton2.setPreferredSize(new DimensionUIResource(92, 25));
 			gridBagConstraints.gridx++;
 			jPanel.add(jButton2, gridBagConstraints);
+			
+			jButton.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+
+					thread1Time = new Thread(thread1);
+					thread1Time.start();
+					
+					thread2Time = new Thread(thread2);
+					thread2Time.start();
+					
+				}
+			});
+			
+			jButton2.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					thread1Time.stop();
+					thread2Time.stop();
+					
+				}
+				
+			});
 			
 			add(jPanel, BorderLayout.WEST);
 			setVisible(true); /*Torna a tela visivel para o usuario, e sempre sera o utimo comando do construtor de telas*/

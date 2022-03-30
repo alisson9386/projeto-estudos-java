3012913eaaa361112fba232a6d60c7ec29d609c5
Alisson Deives <alissondeives70@gmail.com>
2022-03-30 19:25:37 -0300

    Data, anos, dias, meses e parcelas com LocalDate

--- primeiro-programa-java/src/cursojava/date/DatasEmJava.java
+++ primeiro-programa-java/src/cursojava/date/DatasEmJava.java
@@ -19,28 +19,30 @@ public class DatasEmJava {
 	
 	public static void main(String[] args) throws ParseException, InterruptedException {
 		
-		LocalDate dataAntiga = LocalDate.parse("2010-03-07");
+		LocalDate dataBase = LocalDate.parse("2022-03-07");
 		
-		LocalDate dataNova = LocalDate.parse("2021-04-03");
+		/*System.out.println("Mais 5 dias: " + (dataBase.plusDays(5)));
 		
-		System.out.println("Data antiga é maior que data nova? "+dataAntiga.isAfter(dataNova));
+		System.out.println("Mais 5 semanas " + (dataBase = dataBase.plusWeeks(5)));
 		
-		System.out.println("Data antiga é anterior a data nova? "+dataAntiga.isBefore(dataNova));
+		System.out.println("Mais 5 meses " + (dataBase = dataBase.plusMonths(5)));
 		
-		System.out.println("Datas são iguais? " + dataAntiga.isEqual(dataNova));
+		System.out.println("Mais 5 anos " + (dataBase = dataBase.plusYears(5)));
 		
+		System.out.println("Menos 1 ano " + (dataBase = dataBase.minusYears(1)));
 		
-		Period periodo = Period.between(dataAntiga, dataNova);
+		System.out.println("Menos 1 mes " + (dataBase = dataBase.minusMonths(1)));
 		
-		System.out.println("Quantos dias tem a faiza de periodo? "+periodo.getDays());
+		System.out.println("Menos 1 semana " + (dataBase = dataBase.minusWeeks(1)));
 		
-		System.out.println("Quantos meses tem a faiza de periodo? "+periodo.getMonths());
+		System.out.println("Menos 1 dia " + (dataBase = dataBase.minusDays(1)));*/
 		
-		System.out.println("Quantos anos tem a faiza de periodo? "+periodo.getYears());
+		for(int mes = 1; mes <= 12; mes++) {
+			dataBase = dataBase.plusMonths(1);
+			System.out.println("Vencimento do boleto: " + dataBase.format(DateTimeFormatter.ofPattern("dd/MM/yyyy")) + " do mês: " + mes);
+			
+		}
 		
-		System.out.println("Somente em meses: "+periodo.toTotalMonths());
-		
-		System.out.println("Periodo anos: " + periodo.getYears() + " anos, "+periodo.getMonths() + " meses e "+periodo.getDays() + " dias");
 		
 		}
 		

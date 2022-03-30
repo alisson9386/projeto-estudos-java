4ae2cfb37ee891fe5f73783022b677ca4a3c8efa
Alisson Deives <alissondeives70@gmail.com>
2022-03-30 15:22:58 -0300

    Gerando parcelas mensais e suas datas

--- primeiro-programa-java/src/cursojava/date/DatasEmJava.java
+++ primeiro-programa-java/src/cursojava/date/DatasEmJava.java
@@ -7,18 +7,40 @@ import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.swing.JOptionPane;
+
 public class DatasEmJava {
 	
 	public static void main(String[] args) throws ParseException {
-				
-			long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-02-01"), LocalDate.now());
-			
-			System.out.println("Possui "+dias+" dias entre a faixa de data");
-			
-			
-			long decadas = ChronoUnit.DECADES.between(LocalDate.parse("1998-02-01"), LocalDate.now());
-			
-			System.out.println("Possui "+decadas+" decadas entre a faixa de data");
+		
+		String nome = JOptionPane.showInputDialog("Nome do cliente");
+		
+		String produto = JOptionPane.showInputDialog("Produto comprado");
+		
+		double valorProduto = Double.parseDouble(JOptionPane.showInputDialog("Valor do produto"));
+		
+		int parcelaDesejada = Integer.parseInt(JOptionPane.showInputDialog("Número de parcelas"));
+		
+		double valorParcela = valorProduto / parcelaDesejada;
+		
+		Date dataInicial = new SimpleDateFormat("dd-MM-yyyy").parse("30-03-2022");
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(dataInicial);
+		
+		for(int parcela = 1 ; parcela <= parcelaDesejada ; parcela ++) {
+		calendar.add(Calendar.MONTH, 1);
+		
+		System.out.println("Parcela " + parcela + ", vencimento é: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
+		
+		JOptionPane.showMessageDialog(null, "Nome do cliente: " + nome +
+											"\nProduto comprado: " + produto + 
+											"\nValor do produto: " + valorProduto + 
+											"\nNúmero de parcelas: " + parcelaDesejada +
+											"\nDetalhamento das parcelas: " +
+											"Parcela " + parcela + ", vencimento é: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()) + 
+											"\nValor da Parcela: "+valorParcela);
+		}
 		
 		}
 		

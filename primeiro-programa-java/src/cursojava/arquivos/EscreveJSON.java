49d10a5458dcf675201c35e48a063f4505f2daf0
Alisson Deives <alissondeives70@gmail.com>
2022-04-05 18:42:50 -0300

    Escrevendo JSON em arquivo TXT com GSON

---
+++ primeiro-programa-java/src/cursojava/arquivos/EscreveJSON.java
@@ -0,0 +1,47 @@
+package cursojava.arquivos;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class EscreveJSON {
+
+	public static void main(String[] args) throws IOException {
+
+
+		Usuario usuario1 = new Usuario();
+		usuario1.setCpf("13502004684");
+		usuario1.setLogin("alisson");
+		usuario1.setSenha("Adcv9386");
+		usuario1.setNome("Alisson Vieira");
+		
+		Usuario usuario2 = new Usuario();
+		usuario2.setCpf("14461521621");
+		usuario2.setLogin("rafaela");
+		usuario2.setSenha("03011978");
+		usuario2.setNome("Rafaela Restier");
+		
+		List<Usuario> usuarios = new ArrayList<Usuario>();
+		
+		usuarios.add(usuario1);
+		usuarios.add(usuario2);
+		
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String jsonUser = gson.toJson(usuarios);
+		
+		System.out.println(jsonUser);
+		
+		FileWriter fileWriter = new FileWriter("C:\\Users\\Alisson\\Documents\\GitHub\\primeiro-projeto-java\\primeiro-programa-java\\src\\cursojava\\arquivos\\filsejson.json");
+		
+		fileWriter.write(jsonUser);
+		fileWriter.flush();
+		fileWriter.close();
+		
+		
+	}
+
+}

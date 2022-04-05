b75a979eb3aa3454f710a8b7d3cdc945d10dbc07
Alisson Deives <alissondeives70@gmail.com>
2022-04-05 09:30:37 -0300

    Lendo Excel XLS com Apache Poi e montando lista

---
+++ primeiro-programa-java/src/cursojava/arquivos/ApachePoi2.java
@@ -0,0 +1,66 @@
+package cursojava.arquivos;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+
+public class ApachePoi2 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream entrada = new FileInputStream(new File("C:\\Users\\Alisson\\Documents\\GitHub\\primeiro-projeto-java\\primeiro-programa-java\\src\\cursojava\\arquivos\\arquivo_excel.xls"));
+		
+		@SuppressWarnings("resource")
+		HSSFWorkbook hssfWorkbook = new HSSFWorkbook(entrada);
+		
+		HSSFSheet planilha = hssfWorkbook.getSheetAt(0); /*Pega a primeira planilha do arquivo excel*/
+		
+		Iterator<Row> linhaIterator = planilha.iterator();
+		
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		while(linhaIterator.hasNext()) {
+			
+			Row linha = linhaIterator.next();
+			Iterator<Cell> celulas = linha.iterator();
+			
+			Pessoa pessoa = new Pessoa();
+			
+			while(celulas.hasNext()) {
+				
+				Cell cell = celulas.next();
+				switch (cell.getColumnIndex()) {
+				case 0:
+					pessoa.setNome(cell.getStringCellValue());
+					break;
+					
+				case 1:
+					pessoa.setEmail(cell.getStringCellValue());
+					break;
+					
+				case 2:
+					pessoa.setIdade(Double.valueOf(cell.getNumericCellValue()).intValue());
+					break;
+				}
+			}
+			
+			pessoas.add(pessoa);
+		}
+		
+		entrada.close();
+		
+		for (Pessoa pessoa : pessoas) {
+			System.out.println(pessoa);
+		}
+		
+	}
+
+}
